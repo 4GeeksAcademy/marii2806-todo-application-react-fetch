ed65ed045fbfd7f774d87391623142141e469957
Marii2806 <107720808+Marii2806@users.noreply.github.com>
2024-02-14 22:57:44 +0000

    Forst commit TODO BASIC PROJECT

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter" && inputValue.trim() !== "") {
+            e.preventDefault();
+            setTodos([...todos, inputValue.trim()]);
+            setInputValue("");
+        }
+    };
+
+    const handleDeleteTodo = (index) => {
+        setTodos(todos.filter((_, i) => i !== index));
+    };
+
+    return (
+        <div className="container">
+            <h1>todos</h1>
+            <ul className = "container-fluid list-group list-group flush">
+                <li>
+                    <input
+                        type="text"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyDown={handleKeyDown}
+                        placeholder="What I need to do?"
+                    />
+                </li>
+                {todos.map((item, index) => (
+                    <li key={index}>
+                        {item}{" "}
+                        <i
+                            onClick={() => handleDeleteTodo(index)}
+                        >
+                            🗑️
+                        </i>
+                    </li>
+                ))}
+            </ul>
+            <div className = "footer">{todos.length} tasks left</div>
+        </div>
+    );
 };
 
 export default Home;
